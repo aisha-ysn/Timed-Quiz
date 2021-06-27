872cc73412b8c54fc9676000c3538860e0a35423
aisha-ysn <aishayasin1801@gmail.com>
2021-06-27 02:38:51 +0100

    adding script

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,30 +12,20 @@ let inputField = document.getElementById('input-field');
 let initials = document.getElementById('initials');
 let submitButton = document.getElementById('submit-button');
 
-//variables
 let timerSecs = 0;
 let currentQuestion = 0
 let score = 0;
 let scoreArray = [];
 let timerInterval = false;
 
-// start
 function startQuiz() {
-    // timer start at 75 seconds
     timerSecs = 50;
     timerDisplay.textContent = timerSecs;
-
-    //countdown
     countdown();
-
-
     nextQuestion();
-
-    //start button disappear
     startButton.style.display = 'none';
 }
 
-//next question
 function nextQuestion() {
 
     container.className = 'results-page mt-5'
@@ -45,80 +35,67 @@ function nextQuestion() {
     text.className = 'h4';
     text.setAttribute('style', 'border-top: 1px double #ba251a; padding-top: 20px;')
 
-
     quizAnswers.style.display = 'block';
 
-    // assigns new question options
     answerButtons[0].textContent = questions[currentQuestion].choices[0];
     answerButtons[1].textContent = questions[currentQuestion].choices[1];
     answerButtons[2].textContent = questions[currentQuestion].choices[2];
     answerButtons[3].textContent = questions[currentQuestion].choices[3];
 
-    // Clicking answer
     for (i = 0; i < answerButtons.length; i++) {
         answerButtons[i].addEventListener('click', checkAnswer);
     }
 }
 
-// is answer correct?
 function checkAnswer(event) {
-
+ 
     console.log('User chose: ' + event.target.textContent);
     console.log('Correct answer: ' + questions[currentQuestion].answer);
 
-    // if correct displays correct and increases score
     if (event.target.textContent === questions[currentQuestion].answer) {
         answerMessage.style.display = 'block';
         answerMessage.textContent = 'Correct!';
         answerMessage.className = 'answer-message';
         currentQuestion++;
         score++;
-
-        // after set time
-        setTimeout(function () {
+        
+        setTimeout(function() {
             answerMessage.style.display = 'none';
         }, 800);
-
-        // end game after Q5
+            
         if (currentQuestion === questions.length) {
             endGame();
-
-            // otherwise go to next Q
+   
         } else {
             nextQuestion();
         };
 
-        // decrease time by 10 seconds
     } else {
         currentQuestion++;
         answerMessage.style.display = 'block';
         answerMessage.textContent = 'Incorrect!';
         answerMessage.className = 'answer-message';
 
-        // incorrect disappears after the set time
-        setTimeout(function () {
+        setTimeout(function() {
             answerMessage.style.display = 'none';
         }, 800);
 
-        // the game ends if timer is less than 10 seconds
         if (timerSecs < 10) {
             timerSecs -= 10;
             endGame();
 
-            // ends game after Q5
         } else if (currentQuestion === 5) {
             endGame();
 
-            // or minus 10 secs from timer and go to the next question
         } else {
             timerSecs -= 10;
             nextQuestion();
         };
     }
 };
-
-
+// game page
 function endGame() {
+
     quizAnswers.style.display = 'none';
     container.className = 'quiz-page mt-5'
     title.setAttribute('class', 'h2');
@@ -136,13 +113,11 @@ function endGame() {
     submitButton.addEventListener('click', storeHighScore);
 }
 
-
 function storeHighScore(event) {
     event.preventDefault();
 
     if (initials.value.length === 0) {
         return
-
 
     } else {
         newScore = {
@@ -151,28 +126,23 @@ function storeHighScore(event) {
         };
         scoreArray.push(newScore);
 
-
         scoreArray.sort((a, b) => b.userScore - a.userScore);
 
-
         localStorage.setItem('score', JSON.stringify(scoreArray));
-
 
         seeHighScores();
     }
 }
 
 function loadHighScore() {
-
     storedScores = JSON.parse(localStorage.getItem('score'));
-
     if (storedScores !== null) {
         scoreArray = storedScores;
-
         return scoreArray;
     }
+}
 
-
+function seeHighScores() {
     if (timerInterval) {
         clearInterval(timerInterval);
     };
@@ -195,30 +165,26 @@ function loadHighScore() {
     quizAnswers.style.display = 'none';
     inputField.style.display = 'none';
 
-
     for (i = 0; i < scoreArray.length; i++) {
         let score = scoreArray[i].userName + ' : ' + scoreArray[i].userScore;
-
         li = document.createElement('li');
         li.textContent = score;
         ul.appendChild(li);
     }
 
-
-    returnButton.addEventListener('click', function () {
+    returnButton.addEventListener('click', function() {
         location.href = 'index.html'
     });
-
-    clearButton.addEventListener('click', function () {
+   
+    clearButton.addEventListener('click', function() {
         localStorage.clear();
         ul.innerHTML = '';
     });
 };
-
-
+ 
 function countdown() {
 
-    timerInterval = setInterval(function () {
+    timerInterval = setInterval(function() {
         timerSecs--;
         timerDisplay.textContent = timerSecs;
 
@@ -227,7 +193,7 @@ function countdown() {
             endGame();
             clearInterval(timerInterval);
         };
-
+ 
         if (currentQuestion === 5) {
             timerDisplay.textContent = timerSecs;
             clearInterval(timerInterval);
@@ -236,7 +202,7 @@ function countdown() {
 }
 
 function handleFirstTab(e) {
-    if (e.keyCode === 9) {
+    if (e.keyCode === 9) { // the 'I am a keyboard user' key
         document.body.classList.add('user-is-tabbing');
         window.removeEventListener('keydown', handleFirstTab);
     }
@@ -250,28 +216,28 @@ startButton.addEventListener('click', startQuiz);
 
 highscoresLink.addEventListener('click', seeHighScores);
 let questions = [{
-    title: "Commonly used data types DO NOT include:",
-    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-    answer: "3. alerts"
-},
-{
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
-    answer: "3. parentheses"
-},
-{
-    title: "What is the name of the statement that is used to exit or end a loop?",
-    choices: ["1. falter statement", "2. conditional statement", "3. break statement", "4. close statement"],
-    answer: "3. break statement"
-},
-{
-    title: "What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?",
-    choices: ["1. scope", "2. restriction", "3. range", "4. output level"],
-    answer: "1. scope"
-},
-{
-    title: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
-    choices: ["1. conditional loop", "2. for loop", "3. else loop", "4. while loop"],
-    answer: "4. while loop"
-}
+        title: "Commonly used data types DO NOT include:",
+        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "3. alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        answer: "3. parentheses"
+    },
+    {
+        title: "What is the name of the statement that is used to exit or end a loop?",
+        choices: ["1. falter statement", "2. conditional statement", "3. break statement", "4. close statement"],
+        answer: "3. break statement"
+    },
+    {
+        title: "What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?",
+        choices: ["1. scope", "2. restriction", "3. range", "4. output level"],
+        answer: "1. scope"
+    },
+    {
+        title: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
+        choices: ["1. conditional loop", "2. for loop", "3. else loop", "4. while loop"],
+        answer: "4. while loop"
+    }
 ]
